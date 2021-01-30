cf34c9874c907a04724770135ae7fc348afa7be1
ItsKarlito <karlnoory@hotmail.com>
2021-01-30 18:31:41 -0500

    client and server updated code

--- src/Client.java
+++ src/Client.java
@@ -1,42 +1,111 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import javax.swing.*;
 
 public class Client {
     public static void main(String[] args) {
-        try
-        {
+        setupGUI();
+    }
+
+    private static void setupGUI() {
+        JFrame frame = new JFrame();
+        frame.setBounds(100, 100, 455, 165); // set position and dimensions
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //need this to fully terminate application
+        frame.setTitle("UDP Calculator");
+        frame.getContentPane().setLayout(null); //use to correctly draw other components
+
+        // imagine every component as a rectangle
+        JLabel firstNumberLabel = new JLabel(""); // create empty label component
+        firstNumberLabel.setBounds(20, 20, 200, 20); // relative to top left corner of application window
+        frame.getContentPane().add(firstNumberLabel); // add this at end of each component after creating a new component
+        firstNumberLabel.setText("First Number");
+
+        // imagine every component as a rectangle
+        JLabel secondNumberLabel = new JLabel(""); // create empty label component
+        secondNumberLabel.setBounds(20, 45, 200, 20); // relative to top left corner of application window
+        frame.getContentPane().add(secondNumberLabel); // add this at end of each component after creating a new component
+        secondNumberLabel.setText("Second Number");
+
+        // imagine every component as a rectangle
+        JLabel resultLabel = new JLabel(""); // create empty label component
+        resultLabel.setBounds(20, 70, 400, 20); // relative to top left corner of application window
+        frame.getContentPane().add(resultLabel); // add this at end of each component after creating a new component
+        resultLabel.setText("");
+
+        JTextField firstNumberTextField = new JTextField(""); // used to type one line of text
+        firstNumberTextField.setBounds(152, 20, 150, 20);
+        frame.getContentPane().add(firstNumberTextField);
+
+        JTextField secondNumberTextField = new JTextField(""); // used to type one line of text
+        secondNumberTextField.setBounds(152, 45, 150, 20);
+        frame.getContentPane().add(secondNumberTextField);
+
+        JButton addButton = new JButton("+");
+        addButton.setBounds(20, 100, 65, 23);
+        frame.getContentPane().add(addButton);
+
+        JButton subtractButton = new JButton("-");
+        subtractButton.setBounds(90, 100, 65, 23);
+        frame.getContentPane().add(subtractButton);
+
+        JButton multiplyButton = new JButton("x");
+        multiplyButton.setBounds(160, 100, 65, 23);
+        frame.getContentPane().add(multiplyButton);
+
+        JButton divideButton = new JButton("÷");
+        divideButton.setBounds(230, 100, 65, 23);
+        frame.getContentPane().add(divideButton);
+
+        JButton maxButton = new JButton("Max");
+        maxButton.setBounds(300, 100, 65, 23);
+        frame.getContentPane().add(maxButton);
+
+        JButton minButton = new JButton("Min");
+        minButton.setBounds(370, 100, 65, 23);
+        frame.getContentPane().add(minButton);
+
+        addButton.addActionListener(e -> resultLabel.setText(sendMessage(firstNumberTextField.getText() + "," + secondNumberTextField.getText() + "," + 0)));
+        subtractButton.addActionListener(e -> resultLabel.setText(sendMessage(firstNumberTextField.getText() + "," + secondNumberTextField.getText() + "," + 1)));
+        multiplyButton.addActionListener(e -> resultLabel.setText(sendMessage(firstNumberTextField.getText() + "," + secondNumberTextField.getText() + "," + 2)));
+        divideButton.addActionListener(e -> resultLabel.setText(sendMessage(firstNumberTextField.getText() + "," + secondNumberTextField.getText() + "," + 3)));
+        maxButton.addActionListener(e -> resultLabel.setText(sendMessage(firstNumberTextField.getText() + "," + secondNumberTextField.getText() + "," + 4)));
+        minButton.addActionListener(e -> resultLabel.setText(sendMessage(firstNumberTextField.getText() + "," + secondNumberTextField.getText() + "," + 5)));
+
+        frame.setVisible(true); //draw stuff first then show window, prevents issues
+    }
+
+    private static String sendMessage(String msg) {
+        try {
+            String[] messageComponents = msg.split(",");
+            Double.parseDouble(messageComponents[0]); // Use to test if input data is valid
+            Double.parseDouble(messageComponents[1]); // Use to test if input data is valid
+            Double.parseDouble(messageComponents[2]); // Use to test if input data is valid
+
             DatagramSocket clientSocket = new DatagramSocket();
 
             // Send a message to server
-
-            //InetAddress IPAddress = InetAddress.getByName("localhost"); // localhost loopback connection 127.0.0.1
-            InetAddress IPAddress = InetAddress.getByAddress(new byte[] {127,0,0,1});
+            InetAddress IPAddress = InetAddress.getByName("localhost"); //InetAddress IPAddress = InetAddress.getByAddress(new byte[] {127,0,0,1}); // localhost loopback connection 127.0.0.1
             int port = 9876;
 
-            String sendingSentence = "Welcome from Client: " + clientSocket.getLocalPort();
+            byte[] sendingDataBuffer = new byte[1024]; // fill new buffer with reply data and convert to string
+            sendingDataBuffer = msg.getBytes();
 
-            // fill new buffer with reply data and convert to string
-            byte[] sendingDataBuffer = new byte[1024];
-            sendingDataBuffer = sendingSentence.getBytes();
-
-            // create packet for outgoing data
-            DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer, sendingDataBuffer.length, IPAddress, port);
-            // send packet to socket over network
-            clientSocket.send(sendingPacket);
-
+            DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer, sendingDataBuffer.length, IPAddress, port); // create packet for outgoing data
+            clientSocket.send(sendingPacket);// send packet to socket over network
 
             // Receive server reply
             byte[] data = new byte[1024];
             DatagramPacket receivedPacket = new DatagramPacket(data, data.length);
             clientSocket.receive(receivedPacket);
             String receivedSentence = new String(receivedPacket.getData());
-            System.out.println(receivedSentence);
+            System.out.println("Message from Server: " + receivedSentence);
+            return receivedSentence.trim();
 
-        }
-        catch (Exception ex)
-        {
-
+        } catch (NumberFormatException ex) {
+            return "ERROR: Invalid Input";
+        } catch (Exception ignored) {
+            return "ERROR: Unknown";
         }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -4,7 +4,6 @@ import java.net.InetAddress;
 
 public class Server {
     public static void main(String[] args) {
-
         try {
             DatagramSocket serverSocket = new DatagramSocket(9876);
 
@@ -12,34 +11,53 @@ public class Server {
                 // Receive a message from the client
                 byte[] data = new byte[1024]; // buffer with size 1024 bytes
                 DatagramPacket receivedPacket = new DatagramPacket(data, data.length); // create an empty packet of size 1024 bytes, this is an empty datagram packet
-
                 serverSocket.receive(receivedPacket); // fill empty packet with data
-
                 String receivedSentence = new String(receivedPacket.getData()); // extract data part and convert to string
-
 
                 // Extracting the IP address and port number of the client
                 InetAddress IPAddress = receivedPacket.getAddress();
                 int port = receivedPacket.getPort();
 
-                System.out.println("Receiving from Client: " + port + ": " + receivedSentence);
+                System.out.println("Message from Client: " + receivedSentence);
+
+                // Extracting numbers and operation from client message
+                String[] messageComponents = receivedSentence.split(",");
+                double firstNumber = Double.parseDouble(messageComponents[0]);
+                double secondNumber = Double.parseDouble(messageComponents[1]);
+                double operator = Double.parseDouble(messageComponents[2]);
+
+                double result = 0;
+                switch ((int) operator) {
+                    case 0: // add
+                        result = firstNumber + secondNumber;
+                        break;
+                    case 1: // subtract
+                        result = firstNumber - secondNumber;
+                        break;
+                    case 2: // multiply
+                        result = firstNumber * secondNumber;
+                        break;
+                    case 3: // divide
+                        result = firstNumber / secondNumber;
+                        break;
+                    case 4: // max
+                        result = Math.max(firstNumber, secondNumber);
+                        break;
+                    case 5: // min
+                        result = Math.min(firstNumber, secondNumber);
+                        break;
+                    default:
+                        break;
+                }
 
                 // sending a reply
-                String sendingSentence = "Welcome from Server";
-
-                // fill new buffer with reply data and convert to string
-                byte[] sendingDataBuffer = new byte[1024];
+                String sendingSentence = "Answer: " + result;
+                byte[] sendingDataBuffer = new byte[1024]; // fill new buffer with reply data and convert to string
                 sendingDataBuffer = sendingSentence.getBytes();
-
-                // create packet for outgoing data
-                DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer, sendingDataBuffer.length, IPAddress, port);
-                // send packet to socket over network
-                serverSocket.send(sendingPacket);
+                DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer, sendingDataBuffer.length, IPAddress, port);   // create packet for outgoing data
+                serverSocket.send(sendingPacket); // send packet to socket over network
             }
-
-        } catch (Exception ex) {
-
-
+        } catch (Exception ignored) { // general "Exception"
         }
     }
 }
